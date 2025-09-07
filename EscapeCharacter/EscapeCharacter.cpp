3638778c918563b1dd7917db629158934a5c8943
xuezhaju <liaozecheng123@163.com>
2025-09-07 17:43:53 +0800

    9.7 EscapeCharacter

---
+++ EscapeCharacter/EscapeCharacter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!\n";
+    cout << "This is a line with a tab:\tTabbed text.\n";
+    cout << "This is a line with a backslash: \\\n";
+    cout << "This is a line with a double quote: \"Quoted text\"\n";
+    cout << "This is a line with a single quote: \'Single quoted text\'\n";
+    cout << "This is a line with a newline character:\nNew line starts here.\n";
+    return 0;
+}
