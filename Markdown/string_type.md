cb53d9da0a72b0b78fbb826dda6bf5586be83a3e
xuezhaju <liaozecheng123@163.com>
2025-09-07 17:56:06 +0800

    Studied about string_type

---
+++ Markdown/string_type.md
@@ -0,0 +1,8 @@
+### String Type
+
+#### Grammar
+1. In C: char variable_name[] = "worlds";
+2. In C++ : string variable_name[] = "worlds";
+
+- Script
+  [Code](../String/About_string.cpp)
---
+++ String/About_string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string str = "Hello, World!\n";
+    char st[] = "Hello, C++!";
+    cout << str << endl;
+    cout << st << endl;
+    return 0;
+}
