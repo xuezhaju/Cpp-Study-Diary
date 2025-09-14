10d5ad1e3acee2d2310ab08cd857107e9de9aa21
xuezhaju <liaozecheng123@163.com>
2025-09-14 10:53:56 +0800

    increasing

--- About_increasing/increasing.cpp
+++ About_increasing/increasing.cpp
@@ -3,14 +3,22 @@
 using namespace std;
 
 int main() {
+    // 前置递增和后置递增的区别
+    // 前置递增（++a）：先将变量的值加1，然后再使用该变量
     int a = 5;
     int b = 2;
-    b = a++;
+    b = b + ++a;
     ++a;
-    cout << a << " " << b << endl; // 6 5
-    cout << a << endl; // 6
+    cout << a << " " << b << endl; 
+    cout << a << endl;
 
-    // 
+    // 后置递增（a++）：先使用变量的当前值，然后再将变量的值加1
+    int a1 = 5;
+    int b1 = 2;
+    b1 = b1 * a1++;
+    ++a1;
+    cout << a1 << " " << b1 << endl; 
+    cout << a1 << endl;
 
     return 0;
 }
