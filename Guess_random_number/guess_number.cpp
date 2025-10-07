3bda2884843e146c4c4c7c92f41df532c50bb93d
xuezhaju <liaozecheng123@163.com>
2025-10-07 18:58:49 +0800

    guess number update

--- Guess_random_number/guess_number.cpp
+++ Guess_random_number/guess_number.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <ctime>  //这个头文件可以加也可以不加，最新版可以不加
 
 using namespace std ;
 
 int main()
 {
+    //生成随机数要有一个种子，不然就是伪随机数，可以利用系统的当前时间作为种子进行随机数的生成
+    srand( (unsigned int) time(NULL) );
+
     int answer = rand() % 100 + 1;  // rand() % 100 生成随机数 0～99， 在后面+1可以生成1～100
     
     int request = 0;
