883719418c2de627d5b8f1d67e257fc523d6b8d3
xuezhaju <liaozecheng123@163.com>
2025-10-07 17:36:01 +0800

    study logical operator

---
+++ Logical_operator/Logical_operator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a = 4;
+    int b = 10;
+
+    if (a >= 5 || b > 6 ){
+        cout << "YYDS" << endl;
+    }
+   
+    if ( a == 4 && b <= 100 ){
+        cout << "wudil" << endl;
+    }
+
+    if ( a != b ){
+        cout << "666" << endl;
+    }
+
+    return 0;
+
+
+
+}
