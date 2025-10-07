7813159773938e0f09e63d44258ee83e98571908
xuezhaju <liaozecheng123@163.com>
2025-10-07 17:51:52 +0800

    IFs changed

--- About_the_if/IFs.cpp
+++ About_the_if/IFs.cpp
@@ -4,19 +4,19 @@ using namespace std;
 
 int main(){
     int score = 0 ; 
-    cout << "请输入你的分数" << endl;
+    cout << "请输入你的分数" << "你的分数是" << score << endl;
     cin >> score ;
 
     if ( score >= 600){
-        cout << "恭喜录取重高！" << endl ;
+        cout << "恭喜录取重高！" << "你的分数是" << score <<  endl ;
     }
 
     else if (500 <= score <= 599){
-        cout << "恭喜录取普高" << endl ;
+        cout << "恭喜录取普高" << "你的分数是" << score << endl ;
     }
 
     else{
-        cout << "滚去职高吧" << endl ;
+        cout << "滚去职高吧" << "你的分数是" << score << endl ;
     }
     
 
