a2bf077860c8a3c8ef4d8feeef1b09363e0ff509
xuezhaju <liaozecheng123@163.com>
2025-10-07 18:05:50 +0800

    三目运算学习

---
+++ 三目运算符/三目运算符.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a = 10;
+    int b = 6;
+    int c = 0;
+
+    b >= a ? c = b : c = a ;
+
+    cout << c << endl ;
+    
+    return 0 ;
+
+}
