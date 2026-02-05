5240aff15273de1f49008b31df0714b3c81d38bf
学渣驹 <liaozecheng123@163.com>
2026-02-05 10:35:40 +0800

    函数

---
+++ 基础语法部分/函数/结构.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+
+函数返回类型 函数名 (参数1, 参数2){
+
+    函数语句;
+
+    return ; 
+
+}
+
+*/
+
+// 函数的声明
+
+void noting() ;
+
+int main(){
+
+    //mian函数为cpp中的默认的进入函数，是写死的
+
+    noting();
+
+}
+
+void noting()
+{
+    return;
+}
