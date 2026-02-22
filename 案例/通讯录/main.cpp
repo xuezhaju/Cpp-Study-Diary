4f7895189a92a2bd7938429340f02be2e96ad1cc
xuezhaju <liaozecheng123@163.com>
2026-02-22 19:08:40 +0800

    案例1   showMenu

---
+++ 案例/通讯录/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void showMenu()
+{
+    cout << "================" << endl;
+    cout << "==1.添加联系人==" << endl;
+    cout << "==2.显示联系人==" << endl;
+    cout << "==3.删除联系人==" << endl;
+    cout << "==4.查找联系人==" << endl;
+    cout << "==5.修改联系人==" << endl;
+    cout << "==6.清空联系人==" << endl;
+    cout << "==7.退出联系人==" << endl;
+    cout << "================" << endl;
+}
+
+int main()
+{
+    showMenu();
+
+    return 0;
+}
