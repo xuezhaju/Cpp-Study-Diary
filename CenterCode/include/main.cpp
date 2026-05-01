6f55ac1d78db4512144432aa12bdf42342284293
xuezhaju <liaozecheng123@163.com>
2026-05-01 18:29:45 +0800

    include

---
+++ CenterCode/include/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int &b = a;
+    
+    cout << a << endl;
+    cout << b << endl;
+
+    b = 100;
+
+    cout << a << endl;
+    cout << b << endl;
+
+    return 0;
+}
